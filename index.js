dbe856a5354d502c7bedd5235016cb2c2de4391d
Mitch Ullman <james.mitchell@mimbs-ullman.net>
2022-02-20 15:24:21 -0500

    closer to working as I would expect, though I am seeing a 5k instead of 15k rate limit
    Signed-off-by: Mitch Ullman <james.mitchell@mimbs-ullman.net>

--- index.js
+++ index.js
@@ -1,74 +1,76 @@
 import 'dotenv/config'
 // import { createServer } from 'http'
-// import { App, createNodeMiddleware } from '@octokit/app'
+// import { App } from '@octokit/app'
 import { createAppAuth } from '@octokit/auth-app'
 import { Octokit } from '@octokit/core'
 
+// initial Octokit to grab installation id
 const myAuthApp = new Octokit({
   authStrategy: createAppAuth,
   auth: {
     appId: process.env.appId,
-    privateKey: process.env.privateKey
-    // installationId: 171973
-    // oauth: {
-    // clientId: process.env.clientId,
-    // clientSecret: process.env.clientSecret
-  // }
-  // webhooks: {
-  //   secret: 'supersecret'
-  }// }
+    privateKey: process.env.privateKey,
+    oauth: {
+      clientId: process.env.clientId,
+      clientSecret: process.env.clientSecret
+    },
+    webhooks: {
+      secret: 'supersecret'
+    }
+  }
 })
+
+// authenticate and pull JWT for app authentication
+// the base JWT doesn't work with all the endpoints we need
 const appAuth = await myAuthApp.auth({ type: 'app' })
-console.log(appAuth.token)
-// Send requests as GitHub App
-// const { slug } = await myAuthApp.request('GET /user')
-// console.log('authenticated as %s', slug)
+const appToken = appAuth.token
+const myRequest = myAuthApp.request.defaults({
+  headers: {
+    authorization: appToken
+  }
+})
 
-// myAuthApp.webhooks.on('issues.opened', async ({ octokit, payload }) => {
-//   await octokit.request(
-//     'POST /repos/{owner}/{repo}/issues/{issue_number}/comments',
-//     {
-//       owner: payload.repository.owner.login,
-//       repo: payload.repository.name,
-//       issue_number: payload.issue.number,
-//       body: 'Hello World!'
-//     }
-//   )
-// })
-// myAuthApp.webhooks.onAny((id, name, payload) => {
-//   console.log('id: %d name: %s payload: %s', id, name, payload)
-// })
+// pull installations of app to get installation id
+// installation auth requires installation id
+const appInstall = await myRequest('GET /app/installations')
+const appInstToken = appInstall.data[0].id
 
-// createServer(createNodeMiddleware(myAuthApp)).listen(3000)
+// new Octokit using installation authentication
+const github = new Octokit({
+  authStrategy: createAppAuth,
+  auth: {
+    type: 'installation',
+    appId: process.env.appId,
+    privateKey: process.env.privateKey,
+    installationId: appInstToken,
+    refresh: true
+  }
+})
 
-// console.log('authenticated as name: %s, with token: %s', data.name, data.token)
-// import { Octokit } from '@octokit/rest'
+// our call to octocat endpoint
+const octocat = await github.request('GET /octocat')
+  .then((response) => { return response })
 
-// const github = new Octokit()
+// octocat returns an Uin8Array, change to Buffer
+const buf = Buffer.from(octocat.data)
 
-// const githubProps = Object.getOwnPropertyNames(github)
-// const githubRestProps = Object.getOwnPropertyNames(github.rest)
+// print the pretty cat as a string, and show length of buffer
+console.log(
+  `The Cat says:
+  %s
+  \n
+Length of buffer: %d
+  `, buf.toString(), buf.length)
 
-// console.log('Base Property Names: %s', githubProps)
-// console.log('Rest Property Names: %s', githubRestProps)
+const rlTotal = octocat.headers['x-ratelimit-limit']
+const rlUsed = octocat.headers['x-ratelimit-used']
+const rlRemaining = octocat.headers['x-ratelimit-remaining']
+const rlResetAt = octocat.headers['x-ratelimit-reset']
+const rlResetAtFmtd = new Date(Date.now() + (rlResetAt / 1000)) // can't be right
 
-// console.log(Object.getOwnPropertyNames(github.rest))
-// const octocat = await github.request('GET /octocat')
-//   .then((response) => { return response })
-
-// const buf = Buffer.from(octocat.data)
-
-// console.log(
-//   `The Cat says:
-//   %s
-//   \n
-// Length of buffer: %d
-//   `, buf.toString(), buf.length)
-// function printCat (cat) {
-//   return String.fromCharCode.apply(null, new Uint8Array(cat))
-// }
-// console.log(printCat(octocat.data))
-// console.log(`octocat:
-// Status: %d
-// URL: %s
-// Headers: %s\n`, octocat.status, octocat.url, octocat.headers)
+console.log(`
+Rate limit total: ${rlTotal}
+Rate limit used: ${rlUsed}
+Rate limit remaining: ${rlRemaining}
+Rate limit reset at: ${rlResetAtFmtd}
+`)
